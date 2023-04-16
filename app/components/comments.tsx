6248992f1e14f4fc336b5a5a9c6046bf5dd8c77f
August Bright <augustbright@yandex.ru>
2023-04-16 16:02:05 +0400

    Set up comments for posts using Github Discussions

---
+++ app/components/comments.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Giscus from "@giscus/react";
+
+export const Comments = () => (
+    <Giscus
+        id="comments"
+        repo="augustbright/augustbright"
+        repoId="R_kgDOJUsqqw"
+        category="General"
+        categoryId="DIC_kwDOJUsqq84CV0G9"
+        mapping="pathname"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
+        theme="dark"
+        lang="en"
+        loading="eager"
+    />
+);
